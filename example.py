4507b9f9b66a3d12f787b34123b1484cd8cfd2a2
iburenko <iburenko@gmail.com>
2018-07-02 16:57:13 +0300

    new structure

---
+++ example.py
@@ -0,0 +1,28 @@
+import tng.algorithm_backtest.tng as tng
+from datetime import datetime
+
+
+name = "Cornucopia"
+regime = "SP"
+start_date = datetime(2018, 1, 1)
+end_date = datetime(2018, 2, 1)
+
+alg = tng.TNG(name, regime, start_date, end_date)
+
+alg.addInstrument("btcusd")
+alg.addTimeframe("btcusd", 1440)
+
+
+def onBar(instrument):
+    if instrument.open[1] > instrument.close[1]:
+        # If price goes down during the day then sell;
+        alg.sell()
+    elif instrument.open[1] < instrument.close[1]:
+        # If price goes up during the day then buy;
+        alg.buy()
+    else:
+        # If price did not change then do nothing;
+        pass
+
+
+alg.run_backtest(onBar)
