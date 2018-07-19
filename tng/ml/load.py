2c39ee6e6fed6d226af3daf04edc7972c631a6ea
iburenko <iburenko@gmail.com>
2018-07-19 16:36:19 +0300

    new data loader

--- tng/ml/load.py
+++ tng/ml/load.py
@@ -1,10 +1,45 @@
-import numpy as np
 import sys
-sys.path.append('./../../')
+import os
+from datetime import datetime
+import time
+import numpy as np
+import pandas as pd
+#sys.path.append('./../../')
 from tng.algorithm_backtest.tng import TNG
 
+dt = np.dtype({
+    'names': ['time', 'open', 'high', 'low', 'close', 'vol'],
+    'formats':
+    ['uint64', 'float64', 'float64', 'float64', 'float64', 'float64']
+})
 
-def import_data(ticker, timeframe, start_date, end_date):
+where_to_cache = os.path.abspath('.')+'/tng/ml/__cached_history__/'
+
+def import_data(ticker, 
+                timeframe, 
+                start_date, 
+                end_date,
+                reverse = True): 
+    if not isinstance(ticker, str) or \
+        not isinstance(timeframe, int) or \
+        not isinstance(start_date, datetime) or \
+        not isinstance(end_date, datetime):
+        raise TypeError("Check types of arguments!")
+    
+    start_date_str = start_date.strftime("%Y%m%d%H%M%S")
+    end_date_str = end_date.strftime("%Y%m%d%H%M%S")
+    filename = "__"+ticker+str(timeframe)+"_"+start_date_str+"_"+end_date_str+"__"
+    if _is_cached(filename):
+        data = pd.read_csv(where_to_cache+filename, index_col=None, dtype=dt)
+        data = data.to_records(index=False)
+    else:
+        data = _load_data_from_pack(ticker, timeframe, start_date, end_date)
+        if not reverse:
+            data = data[::-1]
+        _cache_data(data, filename)
+    return data
+
+def _load_data_from_pack(ticker, timeframe, start_date, end_date):
     def on_bar(instrument):
         pass
 
@@ -18,117 +53,19 @@ def import_data(ticker, timeframe, start_date, end_date):
     del alg
     return hist
 
-
-def load(fileName,
-         startYear=None,
-         endYear=None,
-         startMonth=1,
-         endMonth=12,
-         startDay=1,
-         endDay=None):
-    readError = False
-    fileOpened = False
-
-    linesRead = 0
-    linesSkipped = 0
-
-    if startYear is None:
-        startYear = 1900
-    if endYear is None:
-        endYear = 2200
-    if endDay is None:
-        endDay = getEndDayOfMonth(endYear, endMonth)
-
-    from datetime import datetime
-
-    startDate = datetime.strptime(
-        str(startYear) + ":" + str(startMonth) + ":" + str(startDay),
-        "%Y:%m:%d")
-    endDate = datetime.strptime(
-        str(endYear) + ":" + str(endMonth) + ":" + str(endDay), "%Y:%m:%d")
-
-    op = []
-    hi = []
-    lo = []
-    cl = []
-    vol = []
-    dtm = []
-
-    try:
-        fileHandle = open(fileName, "r")
-        fileOpened = True
-
-        for line in fileHandle:
-
-            lineSplitted = line.split(",")
-            if len(lineSplitted) < 6:
-                linesSkipped += 1
-                continue
-
-            strDatetime = lineSplitted[0]
-            dateTime = datetime.strptime(strDatetime, '%Y%m%d%H%M%S%f')
-
-            if dateTime < startDate:
-                continue
-            if dateTime > endDate:
-                continue
-
-            dtm.append(dateTime)
-            op.append(float(lineSplitted[1]))
-            hi.append(float(lineSplitted[2]))
-            lo.append(float(lineSplitted[3]))
-            cl.append(float(lineSplitted[4]))
-            vol.append(float(lineSplitted[5].rstrip()))
-
-            linesRead += 1
-    except IOError:
-        readError = True
-
-    if fileOpened:
-        fileHandle.close()
-
-    if readError:
-        return (None)
-
-    op = np.array(op[::-1], dtype='float')
-    hi = np.array(hi[::-1], dtype='float')
-    lo = np.array(lo[::-1], dtype='float')
-    cl = np.array(cl[::-1], dtype='float')
-    vol = np.array(vol[::-1], dtype='float')
-    dtm = dtm[::-1]
-
-    return {
-        'op': op,
-        'hi': hi,
-        'lo': lo,
-        'cl': cl,
-        'vol': vol,
-        'dtm': dtm,
-        'length': linesRead,
-        'skipped': linesSkipped
-    }
-
-
-# end of readFinam
-
-
-def getEndDayOfMonth(year, month):
-    if month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
-        endDay = 31
-    elif month == 2:
-        if year % 4 == 0:
-            if year % 100 == 0:
-                if year % 400 == 0:
-                    endDay = 29
-                else:
-                    endDay = 28
-            else:
-                endDay = 29
-        else:
-            endDay = 28
+def _is_cached(filename):    
+    where_to_cache = os.path.abspath('.')+'/tng/ml/__cached_history__/'
+    cached_files = [
+        file_ for file_ in os.listdir(where_to_cache) if 
+        os.path.isfile((os.path.join(where_to_cache, file_))) and
+        file_.startswith('__')
+    ]
+    if filename in cached_files:
+        return True
     else:
-        endDay = 30
-    return endDay
+        return False
 
-
-# end of def
+def _cache_data(data, filename):
+    where_to_cache = os.path.abspath('.')+'/tng/ml/__cached_history__/'+filename
+    df = pd.DataFrame(data)
+    df.to_csv(where_to_cache, index = False)
